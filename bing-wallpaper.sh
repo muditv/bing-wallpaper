490d2e20830127ab35b398af1a6ce5fd0b7aa6a7
muditv <muditvaidya@gmail.com>
2013-11-09 10:47:50 -0500

    gets wallpapers from bing worldwide.

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 
-PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
-
+PICTURE_DIR="$HOME/Pictures/bing-wallpapers"
 mkdir -p $PICTURE_DIR
+urls=( $(curl http://feeds.feedburner.com/bingimages | grep "enclosure url" | sed 's/<enclosure url="\([^"]*\).*/\1/' ))
 
-urls=( $(curl -s http://www.bing.com | \
-    grep -Eo "url:'.*?'" | \
-    sed -e "s/url:'\([^']*\)'.*/http:\/\/bing.com\1/" | \
-    sed -e "s/\\\//g") )
-
-for p in ${urls[@]}; do
-    filename=$(echo $p|sed -e "s/.*\/\(.*\)/\1/")
+echo "--------- $(date +"%m-%d-%Y %T") --------  "	
+for p in ${urls[@]}; do 
+	filename=$(echo $p | sed -e 's/.*2f\([^_]*\).*/\1/').jpg
     if [ ! -f $PICTURE_DIR/$filename ]; then
-        echo "Downloading: $filename ..."
-        curl -Lo "$PICTURE_DIR/$filename" $p
-    else
-        echo "Skipping: $filename ..."
+        curl -Lo  "$PICTURE_DIR/$filename" $p
+	echo "Downloaded: $filename"
     fi
 done
+echo "---------------------------------------- "
